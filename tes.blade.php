80f6652b9d410ade211afbf317056e886a2ac7ca
BrigadirBima <114201563+BrigadirBima@users.noreply.github.com>
2023-03-03 11:17:44 +0800

    Add files via upload

---
+++ tes.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <nav class="fixed-top h-20 bg-gray-400 shadow-md shadow-gray-500">
+    <div class="grid grid-cols-5 gap-4">
+      <div>
+        <button class="transition ease-in-out delay-50 bg-stone-500 hover:-translate-y-1-x-1 hover:scale-110 hover:bg-stone-600 duration-100 w-20 h-9 rounded ml-5 mt-6">back</button>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+        <p class="text-4xl text-white text-right mr-5 mt-4 ">E-DISCIPLINE</p>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mx-auto my-5 bg-neutral-400">
+    <div class="grid grid-rows-4 grid-cols-4 gap-4">
+      <div class="img row-span-2 rounded bg-stone-800"></div>
+      <div class="text-left text-2xl col-span-2">
+        <ul>
+          <li>Nama : Rapi</li>
+          <li>Kelas : pplg</li>
+          <li>Nis : 9669</li>
+        </ul>
+      </div>
+      <div></div>
+      <div class="text-left text-xl row-span-2">
+        <ul>
+          <li>Ayah : Udin</li>
+          <li>Ibu : Yenny</li>
+          <li>Wali : -</li>
+        </ul>
+      </div>
+      <div class="text-left text-xl row-span-3">
+        <ul>
+          <li>Nama : Rapi</li>
+          <li>Kelas : pplg</li>
+          <li>Nis : 9669</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
